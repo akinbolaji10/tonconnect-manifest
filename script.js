282c99f91babe4ba8d70cc02ca1b60bca5721c5f
Akinbolaji <bolaji9494@gmail.com>
2025-04-16 13:18:53 +0100

    Fix: set correct manifest URL for TonConnect

---
+++ script.js
@@ -0,0 +1,4 @@
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+  manifestUrl: 'https://raw.githubusercontent.com/akinbolaji10/tonconnect-manifest/main/tonconnect-manifest.json',
+  buttonRootId: 'connect-wallet'
+});
