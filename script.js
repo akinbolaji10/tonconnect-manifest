ad74d67a455ce253046d5aa034c48ad8ac86526e
Akinbolaji <bolaji9494@gmail.com>
2025-04-16 21:06:17 +0100

    Update script.js and manifest.json to match new deployment URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-  manifestUrl: 'https://ton-payment-frontend-orj5w9v3a-akinbolajis-projects-20dde10e.vercel.app/tonconnect-manifest.json',
+  manifestUrl: 'https://ton-payment-frontend-ktg30qyt6-akinbolajis-projects-20dde10e.vercel.app/tonconnect-manifest.json',
   buttonRootId: 'connect-wallet'
 });
